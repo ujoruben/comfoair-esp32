54a9368cc699701d572540c34401259d730cbad6
ujoruben <ujoruben@gmail.com>
2026-05-19 18:18:28 +0200

    secrets.h

---
+++ src/secrets.h
@@ -0,0 +1,11 @@
+#define WIFI_SSID "CYR"
+#define WIFI_PASS "LUCIAMARCOS"
+
+#define MQTT_HOST "192.168.1.50"
+#define MQTT_PORT 1883
+#define MQTT_USER ""
+#define MQTT_PASS ""
+#define MQTT_PREFIX "comfoair"
+
+#define CAN_TX_PIN 22
+#define CAN_RX_PIN 23
